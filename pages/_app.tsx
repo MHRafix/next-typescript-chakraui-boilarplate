1b3f96d8bca86c153aa40eedda297522217addae
MHRafix <fmhrafiz27@gmail.com>
2022-09-16 17:44:43 +0600

    biolarplate push:-

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+
+import { extendTheme } from "@chakra-ui/react";
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: "#000",
+    800: "#000",
+    700: "#000",
+  },
+};
+
+const theme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />;
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
